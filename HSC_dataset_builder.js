963562483528731c2e054e6fa82d8428b77da857
Tony <anthony.petrotte@kellerpostman.com>
2024-01-30 10:32:07 -0600

    update

--- HSC_dataset_builder.js
+++ HSC_dataset_builder.js
@@ -2,8 +2,8 @@
     1. Generate the Tags
         -Open the Project in Titan
         -Preview any page
-        -First code will generate a current set of all tags on the pages in the console
-        -Save the generated json on the project as the variable "json_tagset"
+        -First code will generate a current dataset template of all tags on the pages in the console
+        -Save the generated json on the project as the variable "json_fieldset"
 
     2. Use tags to generate dataset and send
         -Use inside JSON nodes in Titan
@@ -11,16 +11,31 @@
         -When dataset for the active page should be generated
         -Encryption for posting out
 */
+
 //-------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------Generating Tags--------------------------------------------------
 //-------------------------------------------------------------------------------------------------------------
 // HSC Tagset Generation
 // Preview any page on the project, run:
 
-var v, tags;
-var tagset={};
+// NEW Fieldset generator to handle repeat container structure
+// Transform the elements object into new fieldset JSON structure
+function genFieldSet(filteredElements) {
+    const transformedObject = {};
+    for (const [elKey, elValue] of Object.entries(filteredElements)) {
 
-// It should work on the live version if you delete the ?ftnocache&device=lg
-// But it's inconsistent
+        transformedObject[elValue.props.tag] = {
+        type: elValue.type,
+        el: elKey,
+        value: elValue.props.lg.value || '',
+        label: elValue.props.lg.label || '',
+        styleRef: elValue.props.styleRef,
+        };
+    }
+    return transformedObject;
+}
+
+var fieldset = {};
 var pages = {"3":"https://kellerpostman.formtitan.com/ftproject/hsc_pfs/ftef4ea233e38740f5abad92545bd68ee5?ftnocache&device=lg",
             "4":"https://kellerpostman.formtitan.com/ftproject/hsc_pfs/ftf6e407f116ad4d94a1c903fb9ebbdd49?ftnocache&device=lg",
             "2":"https://kellerpostman.formtitan.com/ftproject/hsc_pfs/ft047c7e2cbc1f4633abd0e8ecd28e9af0?ftnocache&device=lg",
@@ -28,7 +43,8 @@ var pages = {"3":"https://kellerpostman.formtitan.com/ftproject/hsc_pfs/ftef4ea2
             "5":"https://kellerpostman.formtitan.com/ftproject/hsc_pfs/ftb941bef914be4abb8c326754a05bfb5b?ftnocache&device=lg"};
 
 for (var p in pages){
-    v = fetch(pages[p])
+
+    var v = fetch([pages[p]])
         .then(res=> res.text())
         .then(data=> {
         const appStateStr = data.match(/(?<=__FT__APP__STATE=)(.+?)(?=};<\/script>)/)[0] +"}";
@@ -36,28 +52,130 @@ for (var p in pages){
         if(!appState.pages) return;
         const elements = appState.pages[appState.currentPage]?.Props?.elements;
 
-        if(!elements) return;
-        const tags = Object.keys(elements)
-        .filter(el => elements[el].type === "FormField")
-        .map(el => elements[el]?.props?.tag )
-        .filter(x=>x);
-        return tags;
+        const elementEntries = Object.entries(elements);
+        const filteredElements = elementEntries.reduce((acc, [key, value]) => {
+        if (value.type === 'FormField' || value.type === 'RepeatAutoFit') {
+            acc[key] = value;
+        }
+        return acc;
+        }, {});
+    
+        var fields = genFieldSet(filteredElements);
+        return fields;
     });
 
-    (async()=>{tagset[p] = (await v)})();
+    (async()=>{fieldset[p] = (await v)})()
+}
+//save this string on the project
+//console.log(fieldset);
+
+//Save this string in the variable "json_fieldset" on the Titan project. 
+//-------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------
+
+
+//Alternatively you can run this on the first load of the published site
+//It will work once, but then load empty promises after
+//For some reason the published site is testy
+
+// var fieldset = {};
+// var pages = {"3":"https://kellerpostman.formtitan.com/ftproject/hsc_pfs/ftef4ea233e38740f5abad92545bd68ee5",
+//             "4":"https://kellerpostman.formtitan.com/ftproject/hsc_pfs/ftf6e407f116ad4d94a1c903fb9ebbdd49",
+//             "2":"https://kellerpostman.formtitan.com/ftproject/hsc_pfs/ft047c7e2cbc1f4633abd0e8ecd28e9af0",
+//             "1":"https://kellerpostman.formtitan.com/ftproject/hsc_pfs/ft54dae613916d428dab323a6282b2353c",
+//             "5":"https://kellerpostman.formtitan.com/ftproject/hsc_pfs/ftb941bef914be4abb8c326754a05bfb5b"};
+
+// async function fetchData(){
+//     const promises=[];
+//     for (var p in pages){
+    
+//         var v = fetch([pages[p]])
+//             .then(res=> res.text())
+//             .then(data=> {
+//             const appStateStr = data.match(/(?<=__FT__APP__STATE=)(.+?)(?=};<\/script>)/)[0] +"}";
+//             const appState = JSON.parse(appStateStr);
+//             if(!appState.pages) return;
+//             const elements = appState.pages[appState.currentPage]?.Props?.elements;
+    
+//             const elementEntries = Object.entries(elements);
+//             const filteredElements = elementEntries.reduce((acc, [key, value]) => {
+//             if (value.type === 'FormField' || value.type === 'RepeatAutoFit') {
+//                 acc[key] = value;
+//             }
+//             return acc;
+//             }, {});
+        
+//             return genFieldSet(filteredElements);
+//             //return fields;
+//         });
+//         promises.push(v);
+//     }
+//     const fieldsets = await Promise.all(promises);
+//     return fieldsets;
+// }
+// fetchData()
+//     .then(fieldset => {
+//         console.log(fieldset);
+// })
+
+
+//-------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------Data Handling--------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------
+//Use data on page to populate fieldset
+
+function findTagByEL(elements, idx, page) {
+    for (const [key, value] of Object.entries(elements[page])) {
+        if (value['el']==idx){
+            return key;
+        }
+    }
+    return null;
 }
 
-tagset = JSON.stringify(tagset);
+function getPageValues(fieldset, page){
+    var fields, t;
+    //Have to do the repeat fields first
+    //They do not work the same way
+    for (const [key, value] of Object.entries(fieldset[page])){
+        if (value['type']=='RepeatAutoFit'){
+            fields = ftGetValueByID(ftGetCurrentPage(),value['el']);
+            for (const [k,v] of Object.entries(fields)){
+                t = findTagByEL(fieldset,k,page) ?? null;
+                if (t!=null && fieldset[page].hasOwnProperty(t)){
+                    fieldset[page][t]['value']=v;
+                    //Give these fields new type, otherwise they are overwriten with ''
+                    fieldset[page][t]['type']='RepField';
+                }
+            }
+        }
+    }
 
-//Save this string in the variable "json_tagset" on the Titan project. 
+    for (const [key, value] of Object.entries(fieldset[page])) {
+        if (value['type']=='FormField'){
+            t = ftGetValueByID(ftGetCurrentPage(), value['el']);
+            fieldset[page][key]['value'] = t; 
+        }
+    }
+}
+
+//Vital that this variable is updated for every page on the project
+var page = ftGetParamValue("active_page");
+var fieldset = ftGetParamValue("json_fieldset");
+fieldset = JSON.parse(fieldset);
+var fields = getPageValues(fieldset, page);
+//resave the updated fieldset
+//ftSetParamValue('json_fieldset',JSON.stringify(fieldset));
+
 //-------------------------------------------------------------------------------------------------------------
 //-------------------------------------------------------------------------------------------------------------
 
 
-
-
 //-------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------Sending Data Out-------------------------------------------
 //-------------------------------------------------------------------------------------------------------------
+
+//-------------------------Encryption Stuff----------------------------
 
 //Put the headers in for the package
 //in the Titan -> Tools->Custom Styles
@@ -93,59 +211,74 @@ function getCryptData(data, key_str){
         "data":ct
         };
 }
-//-------------------------------------------------------------------------------------------------------------
 
-
-//Collect the data on the page, encrypt and package
-//Get all data on page
-var tagset = JSON.parse(ftGetParamValue("json_tagset"));
-var page = ftGetParamValue("active_page");
-
-var pagedata = tagset[page].reduce(function(acc,v){
-    acc[v] = ftGetParamValue(v);
-    return acc;
-},{});
-
-pagedata = Object.fromEntries(
-    Object.entries(pagedata).map(([key, value]) => [key, value === undefined ? '' : value])
-);
-
-
+//-------------------------Send Data Somewhere----------------------------
 //encrypt and prep to send
-var key_str = "tMhS+2IsiZmpZcDHW1lNZ6sXDrQamAmxFRazChW8YxM=";
-var crypt = getCryptData(pagedata,key_str);
+//need to have encryption key, dont post in repo
+var key_str = 'NEED A KEY'
+var crypt = getCryptData(fields,key_str);
 
-var sub = {"metadata":{"page":page,
-                        "matter":ftGetParamValue("matter_id"),
-                        "party":ftGetParamValue("party_id"),
+var sub = {"metadata":{"matter":ftGetParamValue("matter_id"),
+                       "party":ftGetParamValue("party_id"),
                         },
             "data":crypt};
 
-
-//sub = JSON.stringify(sub);
 //Send sub wherever
-//-------------------------------------------------------------------------------------------------------------
 
+//Arthur likes Fetch API
+var res;
+const response = fetch("https://pvgsmdqsratrzjjtn3fed3ykli0hwlyv.lambda-url.us-east-1.on.aws/", {
+    method: "POST",
+    mode:"no-cors",
+    headers: {'Access-Control-Allow-Origin':'https://kellerpostman.formtitan.com',
+            'Access-Control-Allow-Credentials':'true'},
+    body: JSON.stringify(sub),
+});
 
-////Send to Tray Webhhok
+// //(async()=>{res = (await response)})()
+
+//HTTP Req
 // var root = "https://3f112ed9-c29c-4dad-b822-605ad0678af5.trayapp.io";
 // var request = new XMLHttpRequest();
 // request.open("POST", root);
-// request.send(JSON.stringify(tagset));    
+// request.send(JSON.stringify(sub));    
+//------------------------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------------------------
 
 
+//------------------------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------------------------
+
+//Utiility to unhide everything on a pager
+// function divdump(elem) {
+//     div = elem.querySelector('div');
+//     if(div){
+//         div.style.display = "flex";
+//         div.classList.remove("ft--elem--hide");
+//         div.classList.remove("disable");
+//     }
+// }
+
+// var elem, div;
+// for (const[k,v] of Object.entries(elements)){
+//     try{
+//         elem = document.getElementById(k);
+//         elem.parentNode.parentNode.classList.remove('ft--elem--hide');
+//         elem.parentNode.parentNode.classList.remove('disable');
+//         elem.parentNode.parentNode.style.display = "flex";
+//         elem.parentNode.classList.remove('ft--elem--hide');
+//         elem.parentNode.classList.remove('disable');
+//         elem.parentNode.style.display = "flex";
+//         elem.style.dispay = "flex";
+//         elem.classList.remove('ft--elem--hide');
+//         elem.classList.remove('disable');
+//         divdump(elem);        
+
+//     }catch(error){};
+// }
 
 
-
-
-
-
-
-
-
-
-
-
-
+//------------------------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------------------------
 
 
